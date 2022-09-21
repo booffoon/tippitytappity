2302d901a44b0d07a0d597269c183f4a16668f51
Jesse Coulson <jccoulson@mail.csuchico.edu>
2022-09-21 11:32:04 -0700

    added correctness analysis

--- main.cpp
+++ main.cpp
@@ -10,12 +10,30 @@ int main(){
     string input;
 
     do{
-        cout << "Type the following phrase and then press return:\n" 
+        cout << "Type the following phrase and then press return:\n"
             << phrase << endl;
 
         getline(cin,input);
 
-        //TODO: Show results here
+        double finalPercentage = 0;
+        int charCounter = 0;
+        for(int i = 0; i < input.length(); i++)
+        {
+          if(input[i] == phrase[i])
+            charCounter +=1;
+        }
+        int stringLengthDiff = 0;
+        if(input.length() > phrase.length())
+          stringLengthDiff = input.length() - phrase.length();
+        else if(input.length() < phrase.length())
+          stringLengthDiff = phrase.length() - input.length();
+
+        if(stringLengthDiff != 0 )
+          charCounter = charCounter - stringLengthDiff*2;
+
+        finalPercentage = (charCounter/phrase.length())*100.0;
+        cout << "Results: " << finalPercentage << "% accurate" << endl;
+
 
         do{
             cout << "Try again? (yes/no)\n";
